ab6f44843dc5381124f0af152a7d6981d5ff9839
rajatweb <rajatsharma.web@gmail.com>
2024-09-28 19:41:54 +0530

    callback route added

---
+++ app/routes/auth.callback/route.tsx
@@ -0,0 +1,18 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
+
+import { login } from "../../shopify.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+
+  if (url.searchParams.get("shop")) {
+    throw redirect(`/app?${url.searchParams.toString()}`);
+  }
+
+  return json({ showForm: Boolean(login) });
+};
+
+export default function App() {
+  return <div>callback</div>;
+}
